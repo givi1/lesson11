6c11c1848c33a0f20948763e81e7d76c8c380617
givi1 <givichargeishvili@gmail.com>
2021-07-09 14:16:45 +0400

    create index.html and script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+let x = 100;
+let y = 12.5;
+let z = "helo word";
+let w = 'georgia';
+let m = true;
+let p = false;
+let k = null;
+let n = undefined;
+
+let r = 5;
+let s = 3.14*r*r;
+console.log('s',s)
+let l = 2*3.14*r;
+console.log('l',l)
+
+let a = 3;
+let b = 4;
+let c = Math.sqrt(a*a + b*b)
+
+let wibo = prompt("sheiyvane wibo");
+let v=wibo*wibo*wibo;
+console.log('kubis moculoba',v);
+
+console.log('kubis moculoba',v);
+
+
